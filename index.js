16e691e32e61c77c59acf44880918210a55a51e0
Aaron Tainter <amtainter@gmail.com>
2024-07-29 15:42:48 -0700

    Add initial action code

---
+++ index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache');
+const io = require('@actions/io');
+
+const workspace = process.env.GITHUB_WORKSPACE;
+const binDir = `${workspace}/bin`;
+
+run().catch(error => {
+    core.setFailed(error.message);
+})
+
+async function run() {
+    switch (process.platform) {
+        case "win32": {
+            const url = 'https://github.com/workos/workos-cli/releases#:~:text=workos_cli_Windows_x86_64.zip';
+            await installZip(binDir, url);
+            break;
+        }
+        case "linux": {
+            const url = 'https://github.com/workos/workos-cli/releases#:~:text=workos_cli_Linux_x86_64.tar.gz';
+            await installTarball(binDir, url);
+            break;
+        }
+        case "darwin": {
+            const url = 'https://github.com/workos/workos-cli/releases#:~:text=workos_cli_Darwin_x86_64.tar.gz';
+            await installTarball(binDir, url);
+            break;
+        }
+        default: {
+            throw new Error(`Unsupported platform '${process.platform}'`);
+        }
+    }
+}
+
+async function installZip(path, url) {
+    await io.mkdirP(path);
+    const downloadPath = await tc.downloadTool(url);
+    await tc.extractZip(downloadPath, path);
+    core.addPath(path);
+}
+
+async function installTarball(path, url) {
+    await io.mkdirP(path);
+    const downloadPath = await tc.downloadTool(url);
+    await tc.extractTar(downloadPath, path);
+    core.addPath(path);
+}
