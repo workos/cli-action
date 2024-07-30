70d9e86c7d63d97b22c11c334529f3b68003ff21
Aaron Tainter <amtainter@gmail.com>
2024-07-30 09:05:13 -0700

    Parameterize version

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
-const io = require('@actions/io');
 
 run().catch(error => {
     core.setFailed(error.message);
 })
 
 async function run() {
+    const version = core.getInput('version');
+    const downloadUrlWithVersion = `https://github.com/workos/workos-cli/releases/${version}/download`
+
+    let cliArch;
+    switch (process.arch) {
+        case "arm64":
+            cliArch = "arm64";
+            break;
+        case "x64":
+            cliArch = "x86_64";
+            break;
+        default:
+            throw new Error(`Unsupported architecture '${process.arch}'`);
+    }
+
     switch (process.platform) {
         case "win32": {
-            const url = `https://github.com/workos/workos-cli/releases/latest/download/workos_cli_Windows_x86_64.zip`;
+            const url = `${downloadUrlWithVersion}/workos_cli_Windows_${cliArch}.zip`;
             await installZip(url);
             break;
         }
         case "linux":
         case "darwin": {
-            const arch = process.arch === "arm64" ? "arm64" : "x86_64";
-            const url = `https://github.com/workos/workos-cli/releases/latest/download/workos_cli_${process.platform}_${arch}.tar.gz`;
+            const url = `${downloadUrlWithVersion}/workos_cli_${process.platform}_${cliArch}.tar.gz`;
             await installTarball(url);
             break;
         }
